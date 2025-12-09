9954f69179df1e0175382fc812780cb8cec97363
fulprimitiv <eremeevdima51@gmail.com>
2025-12-09 22:22:43 +0500

    style: шрифты, сброс стилей

--- frontend-app/src/App.tsx
+++ frontend-app/src/App.tsx
@@ -1,9 +1,15 @@
-function App() {
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+export function App() {
    return (
-      <div>
-         <span>НАЧАЛО РАБОТЫ!</span>
+      <div className="app-wrapper">
+         <span> хедер </span>
+         <div className="app-container">
+            <Routes>
+               <Route path="/" element={<Navigate to="/list" replace />} />
+               <Route path="/list" element={<span> мейн </span>} />
+            </Routes>
+         </div>
       </div>
    );
 }
-
-export default App;
