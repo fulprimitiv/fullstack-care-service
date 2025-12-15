e5d967a377cb99f9e752b20d96a4fc5f6032c647
fulprimitiv <eremeevdima51@gmail.com>
2025-12-16 00:26:20 +0500

    feat: быстрое создание заявки на странице /list

---
+++ frontend-app/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { ActionCard } from '../../components/home/ActionCard/ActionCard';
+import './HomePage.scss';
+
+export const HomePage: React.FC = () => {
+
+	return (
+		<div className="home-page">
+			<ActionCard
+				icon="question"
+				title="Нужна помощь?"
+				description="Опишите, какая помощь вам требуется, и волонтеры откликнутся. 
+				Мы поможем с покупками, уборкой, походом в аптеку и другими бытовыми вопросами."
+				buttonText="Создать заявку на помощь"
+				variant="green"
+			/>
+
+			<ActionCard
+				icon="care"
+				title="Хочу помочь"
+				description="Станьте волонтером и помогайте пожилым людям в вашем районе. 
+				Даже небольшая помощь может значительно улучшить чью-то жизнь."
+				buttonText="Стать волонтёром"
+				variant="orange"
+			/>
+		</div>
+	);
+};
