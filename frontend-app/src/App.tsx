6ba46ed2565e3e33872d725d9ea51f349244d00e
fulprimitiv <eremeevdima51@gmail.com>
2025-12-19 17:21:54 +0500

    feat: страница обработки ошибок

--- frontend-app/src/App.tsx
+++ frontend-app/src/App.tsx
@@ -4,18 +4,22 @@ import { Navigation } from './components/global/Navigation/Navigation';
 import { HomePage } from './pages/HomePage/HomePage';
 import { OrdersPage } from './pages/OrdersPage/OrdersPage';
 import { ProfilePage } from './pages/ProfilePage/ProfilePage';
+import { NotFoundPage } from './pages/NotFoundPage/NotFoundPage';
 
 export function App() {
    return (
       <div className="app-wrapper">
          <Header />
          <Navigation />
+
          <div className="app-container">
             <Routes>
                <Route path="/" element={<Navigate to="/list" replace />} />
                <Route path="/list" element={<HomePage />} />
                <Route path="/profile" element={<ProfilePage />} />
                <Route path="/my-orders" element={<OrdersPage />} />
+               <Route path="/404" element={<NotFoundPage />} />
+               <Route path="*" element={<Navigate to="/404" replace />} />
             </Routes>
          </div>
       </div>
---
+++ frontend-app/src/pages/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './NotFoundPage.scss';
+
+export const NotFoundPage: React.FC = () => {
+	return (
+		<div className="not-found">
+			<div className="not-found__content">
+				<div className="not-found__code">404</div>
+				<h1 className="not-found__title">Страница не найдена</h1>
+				<p className="not-found__text">
+					Возможно, страница была удалена или вы перешли по неверной ссылке.
+				</p>
+
+				<Link to="/list" className="not-found__btn">
+					Вернуться на главную
+				</Link>
+			</div>
+		</div>
+	);
+};
